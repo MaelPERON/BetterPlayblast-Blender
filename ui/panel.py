c56c2732918123c9f7352fa52fd040611c4fd866
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 01:41:03 +0200

    Add(ui/panel.py) implement pop-up menu for Better Playblast panel

--- ui/panel.py
+++ ui/panel.py
@@ -7,3 +7,8 @@ class BP_MainPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+
+def pop_menu(self: bpy.types.Menu, context: bpy.types.Context):
+    layout = self.layout
+    if context.area.show_menus:
+        layout.popover(BP_MainPanel.bl_idname, text="Better Playblast")

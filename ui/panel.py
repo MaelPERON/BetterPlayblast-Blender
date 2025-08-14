379d693bf8d289c94ec3a260727ea88ef0dfb688
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 01:41:56 +0200

    Add(ui/panel.py) register pop-up menu for Better Playblast panel

--- ui/panel.py
+++ ui/panel.py
@@ -12,3 +12,9 @@ def pop_menu(self: bpy.types.Menu, context: bpy.types.Context):
     layout = self.layout
     if context.area.show_menus:
         layout.popover(BP_MainPanel.bl_idname, text="Better Playblast")
+
+def register():
+    bpy.types.VIEW3D_MT_editor_menus.append(pop_menu)
+
+def unregister():
+    bpy.types.VIEW3D_MT_editor_menus.remove(pop_menu)

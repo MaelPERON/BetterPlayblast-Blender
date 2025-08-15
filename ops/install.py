009f2222aeb28c50842e45552f9b24f921836f97
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 03:16:49 +0200

    Update(install.py) enhance bl_description to inform users about potential freezing during installation

--- ops/install.py
+++ ops/install.py
@@ -12,7 +12,7 @@ SITE_PACKAGES = Path(PYTHON).parent.parent / "lib" / "site-packages"
 class InstallMissingPackages(bpy.types.Operator):
 	bl_idname = "bp.install_missing_packages"
 	bl_label = "Install Missing Packages"
-	bl_description = "Install the required packages for Better Playblast"
+	bl_description = "Install the required packages for Better Playblast.\n*This may cause blender to freeze.*\n(Open the console first to see the installation progress)"
 	bl_options = {'REGISTER', 'UNDO'}
 
 	def execute(self, context) -> set[str]:

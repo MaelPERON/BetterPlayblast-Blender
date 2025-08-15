bce8586450b32f9386defc724592e6248f3dffd2
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 03:57:38 +0200

    Update(addon.py) add register function to print addon reload message

--- addon.py
+++ addon.py
@@ -5,3 +5,6 @@ from .BetterPlayblast.install import all_installed
 
 def packages_installed() -> bool:
 	return all_installed(refresh=False)
+
+def register():
+	print("BetterPlayblast-Blender: addon reloaded")

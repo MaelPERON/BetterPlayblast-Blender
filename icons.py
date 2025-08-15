96cfc27d168b4671ea513e5afd80a5ee947cae71
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-15 02:04:26 +0200

    Add(icons.py) implement icon preview registration and management

---
+++ icons.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import bpy.utils.previews as previews
+
+preview_collections = {}
+
+def register():
+	preview_collection = previews.new()
+	preview_collections["main"] = preview_collection
+
+	icon_folder = Path(__file__).parent / "icons"
+	icons = [f for f in icon_folder.iterdir() if f.is_file() and f.suffix in {'.png', '.jpg', '.jpeg', '.svg'}]
+	for icon in icons:
+		preview_collection.load(icon.stem, str(icon), 'IMAGE')
+
+def unregister():
+	for collection in preview_collections.values():
+		previews.remove(collection)
+	preview_collections.clear()

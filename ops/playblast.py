4bc33554efa9e385c56b17b60e7b85c1a4ccd28d
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-16 00:02:02 +0200

    Feat(playblast) save current render settings to a pickle file

--- ops/playblast.py
+++ ops/playblast.py
@@ -1,4 +1,6 @@
 import bpy
+import os
+import pickle
 from pathlib import Path
 from ..BetterPlayblast.install import all_installed
 
@@ -31,4 +33,27 @@ class BP_Playblast(bpy.types.Operator):
 		temp_folder = local_app_data / "BetterPlayblast" / "temp"
 		if not temp_folder.exists():
 			temp_folder.mkdir(parents=True, exist_ok=True)
+
+		# Save all current render settings
+		settings_path = temp_folder / "render_settings.pkl"
+		render_settings = {
+			'filepath': context.scene.render.filepath,
+			'image_settings': {
+				'file_format': context.scene.render.image_settings.file_format,
+				'color_mode': context.scene.render.image_settings.color_mode,
+			},
+			'ffmpeg': {
+				'format': context.scene.render.ffmpeg.format,
+				'codec': context.scene.render.ffmpeg.codec,
+				'gopsize': context.scene.render.ffmpeg.gopsize,
+				'use_max_b_frames': context.scene.render.ffmpeg.use_max_b_frames,
+				'video_bitrate': context.scene.render.ffmpeg.video_bitrate,
+				'maxrate': context.scene.render.ffmpeg.maxrate,
+				'minrate': context.scene.render.ffmpeg.minrate,
+				'buffersize': context.scene.render.ffmpeg.buffersize,
+				'packetsize': context.scene.render.ffmpeg.packetsize,
+				'muxrate': context.scene.render.ffmpeg.muxrate,
+			}
+		}
+		with open(settings_path, "wb") as f: pickle.dump(render_settings, f) # Save .pkl file
 		return {'FINISHED'}

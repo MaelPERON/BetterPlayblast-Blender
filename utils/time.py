aff8fbedf57c3c09db99dc8cb227ab295a9b0dcc
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-16 01:49:37 +0200

    feat(time) create get_timecode_from_frame function

--- utils/time.py
+++ utils/time.py
@@ -8,3 +8,21 @@ def get_current_time(days: bool = True) -> str:
 	"""
 	format = "%Y-%m-%d %H:%M:%S" if days else "%H:%M:%S"
 	return datetime.now().strftime(format)
+
+def get_timecode_from_frame(frame: int, fps: int = 24, start: int = 0) -> str:
+	"""Get the timecode from a frame number.
+
+	Args:
+		frame (int): The frame number.
+		fps (int, optional): The frames per second. Defaults to 24.
+		start (int, optional): The start frame. Defaults to 0.
+
+	Returns:
+		str: The timecode formatted as HH:MM:SS.mmm.
+	"""
+	frame_time = (frame - start) / fps
+	hours = int(frame_time // 3600)
+	minutes = int((frame_time % 3600) // 60)
+	seconds = int(frame_time % 60)
+	milliseconds = int((frame_time - int(frame_time)) * 1000)
+	return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds:03}"

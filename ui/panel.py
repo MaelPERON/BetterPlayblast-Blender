629e800ae30a4fd1c4793f522c6e3dd20c47d6b6
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-16 03:44:16 +0200

    update(panel.py) add BP_Playblast operator

--- ui/panel.py
+++ ui/panel.py
@@ -2,7 +2,9 @@ import bpy
 
 from . import pop_panel_decorator
 from ..addon import packages_installed
+from ..icons import get_icon
 from ..ops.install import InstallMissingPackages
+from ..ops.playblast import BP_Playblast
 
 class BP_MainPanel(bpy.types.Panel):
     bl_label = "Better Playblast"
@@ -13,7 +15,11 @@ class BP_MainPanel(bpy.types.Panel):
     def draw(self, context: bpy.types.Context):
         layout = self.layout
         if not packages_installed():
-            self.draw_install(context, layout)
+            return self.draw_install(context, layout)
+        
+        layout.label(text="Ready to use!", icon='FAKE_USER_ON')
+        row = layout.row()
+        row.operator(BP_Playblast.bl_idname, text=BP_Playblast.bl_label, icon_value=get_icon("logo_operator"))
 
     def draw_install(self, context: bpy.types.Context, layout: bpy.types.UILayout):
         box = layout.box()

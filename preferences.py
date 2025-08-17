f6c2219437e4b021480036ed96d2eb377548c0d8
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-17 17:53:37 +0200

    add(preferences.py) implement playblast filename options in preferences

--- preferences.py
+++ preferences.py
@@ -1,3 +1,4 @@
+from re import sub
 import bpy
 import os
 
@@ -28,7 +29,36 @@ class BP_Preferences(bpy.types.AddonPreferences):
 		subtype='DIR_PATH'
 	)
 
-	def draw(self, context):
+	pb_filename: bpy.props.EnumProperty( # type: ignore
+		name="Playblast Filename",
+		description="Choose the filename for playblasts",
+		items=[
+			("FILE_NAME", "Blender Filename", "Use the Blender filename"),
+			("CUSTOM", "Custom", "Use a custom filename"),
+			("RENDER", "Same as Output Path", "Use the same name as the output file")
+		],
+		default="RENDER"
+	)
+
+	pb_filename_blender: bpy.props.StringProperty( # type: ignore
+		name="Blender Filename",
+		description="Blender filename",
+		default=""
+	)
+
+	pb_filename_render: bpy.props.StringProperty( # type: ignore
+		name="Render Filename",
+		description="Set the render filename for playblasts",
+		default=""
+	)
+
+	pb_filename_custom: bpy.props.StringProperty( # type: ignore
+		name="Custom Filename",
+		description="Set a custom filename for playblasts",
+		default=""
+	)
+
+	def draw(self, context: bpy.types.Context):
 		layout = self.layout
 
 		box = layout.box()
@@ -44,3 +74,21 @@ class BP_Preferences(bpy.types.AddonPreferences):
 				row.prop(self, "pb_folder_relative", text="")
 			case "CUSTOM":
 				row.prop(self, "pb_folder_custom", text="")
+
+		# Playblast Filename
+		col = box.column(align=True)
+		row = col.row(align=True)
+		row.prop(self, "pb_filename")
+
+		sub_row = row.row(align=True)
+		sub_row.enabled = self.pb_filename == "CUSTOM"
+
+		self.pb_filename_blender = bpy.path.basename(bpy.data.filepath) or "playblast"
+		self.pb_filename_render = bpy.path.basename(context.scene.render.filepath) or "playblast"
+		match self.pb_filename:
+			case "FILE_NAME":
+				sub_row.prop(self, "pb_filename_blender", text="")
+			case "RENDER":
+				sub_row.prop(self, "pb_filename_render", text="")
+			case "CUSTOM":
+				row.prop(self, "pb_filename_custom", text="")

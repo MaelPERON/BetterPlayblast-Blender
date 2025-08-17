64312480d89209e810337918fccd7e7e92778c69
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-17 17:42:47 +0200

    add(preferences.py) base draw method for Playblast settings layout

--- preferences.py
+++ preferences.py
@@ -3,3 +3,9 @@ import os
 
 class BP_Preferences(bpy.types.AddonPreferences):
 	bl_idname = __package__ or "BetterPlayblast-Blender"
+	def draw(self, context):
+		layout = self.layout
+
+		box = layout.box()
+		box.label(text="Playblast Settings", icon="TOOL_SETTINGS")
+		box.separator()

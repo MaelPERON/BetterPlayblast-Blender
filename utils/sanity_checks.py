fdd08d6083fe814772a9874f99560afa7cce6ac4
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-17 18:27:06 +0200

    add(sanity_checks.py) implement SanityCheck class for value validation

---
+++ utils/sanity_checks.py
@@ -0,0 +1,16 @@
+class SanityCheck:
+	def __init__(self, check_function: callable = None, error_message: str = None):
+		self.check_function = check_function
+		self.error_message = error_message or "Invalid value"
+
+	def check(self, value, *args, **kwargs) -> bool:
+		if not self.check_function:
+			return None
+
+		return self.check_function(value, *args, **kwargs)
+
+	def check_and_report(self, value, *args, **kwargs):
+		if not self.check_function:
+			return True, None
+
+		return (self.check_function(value, *args, **kwargs), self.error_message)

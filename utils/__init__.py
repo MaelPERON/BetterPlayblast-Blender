a41345956c9ed47e6b378f2985d09dc7b7963e94
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-20 00:24:20 +0200

    update(__init__.py) refactor and enhance user site package management functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,20 @@
 import os
+import sys
 from importlib import reload
+from site import getusersitepackages
 
 def reload_pyppeteer():	
 	os.putenv("PYPPETEER_CHROMIUM_REVISION", "1230501")
-	import pyppeteer.chromium_downloader as pyppeteer_downloader
-	reload(pyppeteer_downloader)
+	try:
+		import pyppeteer.chromium_downloader as pyppeteer_downloader
+		reload(pyppeteer_downloader)
+	except:
+		pass
+
+def get_user_site_packages() -> str:
+	return str(getusersitepackages())
+
+def add_user_site_packages():
+	site_packages = get_user_site_packages()
+	if site_packages not in sys.path:
+		sys.path.append(site_packages)

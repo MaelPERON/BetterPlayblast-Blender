c34dc742e98d1f4b50d964148b7962511e81ac05
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 18:06:17 +0200

    remove: testing lines

--- __init__.py
+++ __init__.py
@@ -11,7 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from .tests import pip_show
 from . import auto_load
 
 bl_info = {
@@ -24,8 +23,6 @@ bl_info = {
     "warning": "",
     "category": "Generic",
 }
-
-pip_show.main()
 
 auto_load.init()
 

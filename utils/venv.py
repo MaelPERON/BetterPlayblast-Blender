f218f7768c26092525d7bb3fb6b62243d0703bd0
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 18:13:39 +0200

    feat: add functions to retrieve virtual environment binary and Python executable paths

--- utils/venv.py
+++ utils/venv.py
@@ -1,5 +1,7 @@
 import venv
 import site
+import os
+import subprocess
 from pathlib import Path
 
 
@@ -15,3 +17,19 @@ def venv_create() -> Path:
         builder.create(venv_path)
 
     return venv_path
+
+
+def venv_bin() -> Path:
+    venv_path = get_venv_path()
+    if os.name == "nt":
+        return venv_path / "Scripts"
+    else:
+        return venv_path / "bin"
+
+
+def venv_python_executable() -> Path:
+    python_executable = venv_bin() / "python"
+    if os.name == "nt":
+        return python_executable.with_suffix(".exe")
+    else:
+        return python_executable

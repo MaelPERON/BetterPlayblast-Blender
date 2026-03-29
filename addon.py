a77ff81726c29acb580bad83512e8b343d6cec58
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 22:28:18 +0200

    update: extended packages_installed function

--- addon.py
+++ addon.py
@@ -1,8 +1,23 @@
+import os
+from .utils.venv import VenvManager, get_venv_path
+from .utils import chromium_is_installed
 from .BetterPlayblast.install import all_installed
 
 
 def packages_installed() -> bool:
-    return all_installed(refresh=False)
+    # Set the Chromium revision to use
+    os.environ["PYPPETEER_CHROMIUM_REVISION"] = "1230501"
+
+    if not get_venv_path().exists():
+        return False
+
+    if not all_installed(refresh=True):
+        return False
+
+    if not chromium_is_installed():
+        return False
+
+    return True
 
 
 def register():

41e6684183c76e6df07a61c48377d9058f2875cd
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 22:30:14 +0200

    refactor: installation logic

--- ops/install.py
+++ ops/install.py
@@ -1,10 +1,10 @@
 import bpy
 import sys
-from subprocess import CalledProcessError, run
 
-from ..addon import packages_installed
-from ..utils import pyppeteer_download, get_python_executable
-from ..BetterPlayblast.install import missing_packages
+from ..addon import VenvLibraries, packages_installed
+from ..utils import pyppeteer_download
+from ..utils.venv import VenvManager
+from ..BetterPlayblast.install import PACKAGES
 
 PYTHON = sys.executable
 SITE_PACKAGES = bpy.utils.user_resource('SCRIPTS', path="modules")
@@ -21,28 +21,14 @@ class BP_PackageInstaller(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context) -> set[str]:
-        packages = missing_packages(save_cache=False)
+        packages = list(PACKAGES.keys())
 
         # In case they're already installed (encourager the user to restart ->
         # better UX)
         if len(packages) != 0:
-            # Check if python 3.11 is available
-            python_path = get_python_executable("311")
-            if not python_path:
-                self.report({'ERROR'}, "Python 3.11 is not available.")
-                return {'CANCELLED'}
-
-            # Install missing packages (targeting the user site-packages)
-            packages.append("psutil")
-            args = [python_path, '-m', 'pip', 'install', '--user', *packages]
-            print(f"Running command: {' '.join(args)}")
-            proc = run(args)
-            try:
-                proc.check_returncode()
-            except CalledProcessError as e:
-                self.report({'ERROR'}, str(e))
-                self.report({'ERROR'}, "Failed to install packages.")
-                return {'CANCELLED'}
+            venv_manager = VenvManager()
+            venv_manager.install_library(packages)
+            venv_manager.ensure_site()
         else:
             self.report(
                 {'WARNING'},

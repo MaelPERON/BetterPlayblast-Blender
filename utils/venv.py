e58c4ea8dedcfae1609601f19f72545c9fefdd34
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 18:15:08 +0200

    feat: add parse_library_argument function to handle library input types

--- utils/venv.py
+++ utils/venv.py
@@ -33,3 +33,13 @@ def venv_python_executable() -> Path:
         return python_executable.with_suffix(".exe")
     else:
         return python_executable
+
+
+def parse_library_argument(library: str | list[str]) -> list[str]:
+    if isinstance(library, str):
+        return [library]
+    elif isinstance(library, list):
+        return library
+    else:
+        raise ValueError("Library argument must be a string or a list of strings.")  # noqa: E501
+

421c4df2155507d8232794f73a4c06858948ebf7
MaelPERON <maelperonmessagerie2015@gmail.com>
2026-03-29 23:48:20 +0200

    feat: add BP_PackageInstaller draft UI

--- ops/install.py
+++ ops/install.py
@@ -56,3 +56,49 @@ class BP_PackageInstaller(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         return not packages_installed()
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="BetterPlayblast requires additional packages to work.")  # noqa: E501
+
+        # Listing the missing packages.
+        venv = (VenvManager()
+                if get_venv_path().exists()
+                else None)
+
+        subheader, subbody = layout.panel(idname=f"{self.bl_idname}_packages",
+                                          default_closed=True)
+        subheader.label(text="Required Packages:")
+        for package in PACKAGES.keys():
+            if subbody is None:
+                continue
+            box = subbody.box()
+            if venv is not None and venv.has_library(package):
+                box.label(text=package, icon="SORTTIME")
+            else:
+                box.label(text=package)
+
+        # Warning about the installation process.
+        warning_row = layout.row()
+        warning_column = layout.column()
+        warning_column.scale_x = 1.5
+
+        warning_row.alert = True
+        warning_row.label(text="Warning:", icon='WARNING_LARGE')
+
+        messages = [
+            "The installation process may take a while.",
+            "Blender may become unresponsive during installation.",
+            "It's recommended to open the console to see the progress.",
+            "After installation, restart Blender to apply changes.",]
+
+        for message in messages:
+            row = warning_column.row(align=True)
+            row.label(text="", icon='DOT')
+            box = row.box()
+            box.label(text=message)
+            box.emboss = "NONE"
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self, confirm_text="Install")
